ffdad85b662dbc5f3848f4c1c6e66a4701928925
kecinzer <http://kecinzer.cz>
2015-10-27 20:52:11 +0200

    Battery Style and percentage (2/2) (Squashed)
    Adapted this commit: https://github.com/zephiK/android_packages_apps_Settings/commit/9b4f0cf8d92a878586f984e38c01b9c9a418430d
    by zephiK
    
    Original Squashed message by kecinzer:
    Battery text mode (2/2)
    PS5: More configuration
    PS7: Rebased and changed topic
    
    Squashed:
    Author: EnderBlue
    Forward port battery icon option [2/2]
    CyanogenMod/android_packages_apps_Settings@ba62228
    
    Author: mikeNG
    Cleanup battery style code
    CyanogenMod/android_packages_apps_Settings@0a10a1a
    
    Author: ezio84
    Battery style: fix dynamic percentage setting
    CyanogenMod/android_packages_apps_Settings@31dbb48
    
    Author: LorDClockaN
    Settings: Dotted circle battery (2/2)
    AICP/packages_apps_Settings@2c1692a
    
    Change-Id: I13bdf2236811f535b0ac324b5b78c0ab12f365a6
    
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- src/com/android/settings/krexus/UISettings.java
+++ src/com/android/settings/krexus/UISettings.java
@@ -1,6 +1,11 @@
 package com.android.settings.krexus;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.format.DateFormat;
+import android.view.Menu;
+import android.widget.EditText;
 
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.DropDownPreference;
@@ -15,9 +20,19 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class UISettings extends SettingsPreferenceFragment {
+public class UISettings extends SettingsPreferenceFragment
+	implements OnPreferenceChangeListener {
 
     private static final String TAG = "UISettings";
+
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+    private static final int STATUS_BAR_BATTERY_STYLE_HIDDEN = 4;
+    private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 6;
+
+    private ListPreference mStatusBarBattery;
+    private ListPreference mStatusBarBatteryShowPercent;
 
     @Override
     protected int getMetricsCategory() {
@@ -29,5 +44,59 @@ public class UISettings extends SettingsPreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.krexus_ui_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        mStatusBarBatteryShowPercent =
+                (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+
+        int batteryStyle = Settings.Secure.getInt(resolver,
+                Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0);
+        mStatusBarBattery.setValue(String.valueOf(batteryStyle));
+        mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
+        mStatusBarBattery.setOnPreferenceChangeListener(this);
+
+        int batteryShowPercent = Settings.Secure.getInt(resolver,
+                Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
+        mStatusBarBatteryShowPercent.setValue(String.valueOf(batteryShowPercent));
+        mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntry());
+        enableStatusBarBatteryDependents(batteryStyle);
+        mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mStatusBarBattery) {
+            int batteryStyle = Integer.valueOf((String) newValue);
+            int index = mStatusBarBattery.findIndexOfValue((String) newValue);
+            Settings.Secure.putInt(
+                    resolver, Settings.Secure.STATUS_BAR_BATTERY_STYLE, batteryStyle);
+            mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
+
+            enableStatusBarBatteryDependents(batteryStyle);
+            return true;
+        } else if (preference == mStatusBarBatteryShowPercent) {
+            int batteryShowPercent = Integer.valueOf((String) newValue);
+            int index = mStatusBarBatteryShowPercent.findIndexOfValue((String) newValue);
+            Settings.Secure.putInt(
+                    resolver, Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT, batteryShowPercent);
+            mStatusBarBatteryShowPercent.setSummary(
+                    mStatusBarBatteryShowPercent.getEntries()[index]);
+            return true;
+        }
+        return false;
+    }
+
+    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+        if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_HIDDEN ||
+                batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT) {
+            mStatusBarBatteryShowPercent.setEnabled(false);
+        } else if ((Settings.Secure.getInt(getActivity().getContentResolver(), Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT, 0)) != STATUS_BAR_SHOW_BATTERY_PERCENT_INSIDE) {
+            mStatusBarBatteryShowPercent.setEnabled(true);
+        } else {
+            mStatusBarBatteryShowPercent.setEnabled(true);
+        }
     }
 }
