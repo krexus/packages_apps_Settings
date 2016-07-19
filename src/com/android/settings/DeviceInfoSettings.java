b6ce27052fb8ebcf74f214452b03fff782c6d482
beanstown106 <nbenis106@gmail.com>
2016-07-19 01:06:30 -0400

    Dynamically define currently installed vendor.img version
    * parse the vendor fingerprint for the vendor build id
    * automatically remove on devices without a vendor image
    
    Thanks to @dwitherell for the help with the parsing logic :)
    
    Change-Id: Icf956651f53ce8867657d7944b26bb4d57b9e76c

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -76,6 +76,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+    private static final String KEY_VENDOR_VERSION = "vendor_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -117,6 +118,14 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             getPreferenceScreen().removePreference(findPreference(KEY_SECURITY_PATCH));
         }
 
+        String vendorfingerprint = SystemProperties.get("ro.vendor.build.fingerprint");
+        if (vendorfingerprint != null && !TextUtils.isEmpty(vendorfingerprint)) {
+            String[] splitfingerprint = vendorfingerprint.split("/");
+            String vendorid = splitfingerprint[3];
+            setStringSummary(KEY_VENDOR_VERSION, vendorid);
+        } else {
+            getPreferenceScreen().removePreference(findPreference(KEY_VENDOR_VERSION));
+        }
         setValueSummary(KEY_BASEBAND_VERSION, "gsm.version.baseband");
         setStringSummary(KEY_DEVICE_MEMORY, DeviceInfoUtils.getDeviceMemoryInfo());
         setStringSummary(KEY_DEVICE_PROCESSOR, DeviceInfoUtils.getDeviceProcessorInfo(getActivity()));
