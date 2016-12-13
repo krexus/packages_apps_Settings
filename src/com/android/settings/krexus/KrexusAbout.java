74d719092fbc6c618918370ad3fd2a35f5010812
George G <kreach3r@users.noreply.github.com>
2016-12-13 06:20:45 +0200

    KrexusAbout: add Maintainer URL and cleanup code

--- src/com/android/settings/krexus/KrexusAbout.java
+++ src/com/android/settings/krexus/KrexusAbout.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 Krexus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.settings.krexus;
 
 import android.content.Context;
@@ -17,7 +33,6 @@ public class KrexusAbout extends SettingsPreferenceFragment {
 
     private static final String SCREEN_KREXUS_ABOUT = "screen_krexus_about";
     private static final String CATEGORY_KREXUS_MAINTAINER = "category_krexus_about_maintainer";
-    private static final String KEY_KREXUS_MAINTAINER = "krexus_about_maintainer";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,8 +45,6 @@ public class KrexusAbout extends SettingsPreferenceFragment {
                 findPreference(SCREEN_KREXUS_ABOUT);
         final PreferenceGroup krexusMaintainerCategory = (PreferenceGroup)
                 findPreference(CATEGORY_KREXUS_MAINTAINER);
-        final Preference krexusMaintainerPref = (Preference)
-                findPreference(KEY_KREXUS_MAINTAINER);
 
         final String maintainerTitle = getString(R.string.krexus_about_maintainer_title);
 
